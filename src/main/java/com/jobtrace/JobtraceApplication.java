d2680794a6a5e28dfdea7f5daa5d0951533d1c74
egg9402 <limjonini@gmail.com>
2026-03-28 21:36:04 +0900

    feat: 프로젝트 초기 세팅

---
+++ src/main/java/com/jobtrace/JobtraceApplication.java
@@ -0,0 +1,13 @@
+package com.jobtrace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobtraceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobtraceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jobtrace/JobtraceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobtrace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobtraceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
