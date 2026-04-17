4de01554499f0497d157555d027639349eac0517
egg9402 <limjonini@gmail.com>
2026-04-17 09:00:00 +0900

    fix: 에러 코드별 http 상태코드 반환 수정

--- src/main/java/com/jobtrace/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/jobtrace/global/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.jobtrace.global.exception;
 
 import com.jobtrace.global.dto.ErrorResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -10,10 +11,22 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(CustomException.class)
     public ResponseEntity<ErrorResponse> handlerCustomException(CustomException e){
+
+        ErrorCode errorCode = e.getErrorCode();
         return ResponseEntity
-                .badRequest()
-                .body(ErrorResponse.of(e.getErrorCode()));
+                .status(getHttpStatus(errorCode))
+                .body(ErrorResponse.of(errorCode));
     }
+
+    private HttpStatus getHttpStatus(ErrorCode errorCode) {
+        return switch(errorCode) {
+            case DUPLICATE_EMAIL -> HttpStatus.CONFLICT; //409
+            case USER_NOT_FOUND, POST_NOT_FOUND -> HttpStatus.NOT_FOUND; //404
+            case INVALID_PASSWORD -> HttpStatus.BAD_REQUEST; //400
+            case TOKEN_NOT_FOUND,UNAUTHORIZED -> HttpStatus.UNAUTHORIZED; //401
+        };
+    }
+
 
 
 
