fce9e2be29338a9c02094e2fa3850fcf1e5019ed
egg9402 <limjonini@gmail.com>
2026-05-10 21:08:19 +0900

    feat: StatusType 상수 클래스 추가

---
+++ src/main/java/com/jobtrace/global/common/StatusType.java
@@ -0,0 +1,14 @@
+package com.jobtrace.global.common;
+
+public class StatusType {
+    public static final String BOOKMARKED = "관심공고";
+    public static final String APPLYING = "지원중";
+    public static final String DOCUMENT_PASS = "서류합격";
+    public static final String CODING_TEST = "코딩테스트";
+    public static final String APTITUDE_TEST = "인적성";
+    public static final String AI_INTERVIEW = "AI면접";
+    public static final String INTERVIEW_1 = "1차면접";
+    public static final String INTERVIEW_2 = "2차면접";
+    public static final String FINAL_PASS = "최종합격";
+    public static final String FAIL = "탈락";
+}
